ff52b0fbe05f8394a583f0dc8eb15c73357e8a31
Umit Kusdogan <umit@kusdogan.com>
2018-01-11 22:28:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a,b){
+  return(a + b);
+}
+
+function subtract(a,b){
+  return(a - b);
+}
+
+function multiply(a,b){
+  return(a * b);
+}
+
+function divide(a,b){
+  return(a / b);
+}
+
+function inc(n){
+  n +=1;
+  return(n);
+}
+
+function dec(n){
+  n -=1;
+  return(n);
+}
+
+function makeInt(n){
+  parseInt("2",n);
+  return(n);
+}
+
+function makeInt(n){
+  var x = parseInt(n, 10);
+  return(x);
+}
+
+function preserveDecimal(n){
+  parseFloat(n);
+}
+
+function preserveDecimal(n){
+  return(parseFloat(n));
+}
